78326ea6e69d871bf0808f17b61d0a626539ad52
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-08-18 04:30:37 +0100

    main now extends to 100vh

--- pages/fakedata.jsx
+++ pages/fakedata.jsx
@@ -3,7 +3,7 @@ import ReactTable from "../components/reactTable";
 
 const FakeData = () => {
   return (
-    <div className="w-full bg-slate-900">
+    <div className="w-screen bg-slate-900 h-full">
       <h1 className="text-4xl text-center p-8 text-white font-bold">
         Fake Data Dashboard
       </h1>
