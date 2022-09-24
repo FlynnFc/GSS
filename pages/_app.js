2d5962a8652f028d5a0f588b40e7843e6e3e42c2
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-09-24 22:05:58 +0100

    title and icon change

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-slate-200 h-screen">
+      <Head>
+        <title>GSS Dashboad</title>
+      </Head>
+
       <Component {...pageProps} />
     </div>
   );
